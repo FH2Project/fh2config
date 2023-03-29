c11f146d98bec1a3f6ddaf55cf697dec2e4d613e
ian4366691 <i.sayer@auckland.ac.nz>
2023-03-30 09:10:37 +1300

    more knobs and dupli

--- src/utils.mjs
+++ src/utils.mjs
@@ -3,6 +3,59 @@ export let tblDefs = {};
 export function setTblDef(obj){
     tblDefs = Object.assign(tblDefs, obj);
 }
+function updateKnobValue(knob, v)
+{
+    knob.setAttribute("data-value", v);
+    // knob.style=`--k-rotate: ${(v-64)*5}`;
+}
+function clickKnob(e){
+    // updateKnobValue(this, Math.floor(Math.random()*128));
+    updateKnobValue(this, e.shiftKey?++this.dataset.value:--this.dataset.value);
+}
+function scrollKnob(e){
+    e.preventDefault();
+    let v = this.dataset.value>>0;
+    v += e.deltaY * -0.01;
+    v = Math.max(0, v);
+    v = Math.min(127, v);
+    this.dataset.value = v;
+}
+document.addEventListener("DOMContentLoaded", function(){
+    let dups = this.querySelectorAll("[data-duplicate]");
+    dups.forEach(function(ele){
+        let count = ele.dataset.duplicate>>0;
+        ele.removeAttribute("data-duplicate");
+        while (--count){
+            let newele = ele.cloneNode(true);
+            ele.parentNode.insertBefore(newele,ele.nextElementSibling);
+        }
+    });
+    let knobs = document.querySelectorAll(".knob");
+    knobs.forEach(knob=>{
+        knob.onclick = clickKnob;
+        knob.onwheel = scrollKnob;
+        updateKnobValue(knob, knob.classList.length==2?64:0);
+    });
+});
+const muCallback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (mutation.attributeName=='data-value'){
+            let t = mutation.target;
+            let v = t.dataset.value;
+            t.style=`--k-rotate: ${(v-64)*-2.25}`;
+            if (t.classList.contains('note'))
+                t.ariaDescription = "CĊDḊEFḞGĠAȦB"[v%12]+(Math.floor(v/12)-2);
+            else if (t.classList.contains('bipole'))
+                t.ariaDescription=(v-64);
+            else
+                t.ariaDescription=v;
+        }
+    }
+};
+
+const observer = new MutationObserver(muCallback);
+observer.observe(watchKnobs,{attributes: true, subtree: true});
+
 function setSelected(e){
     let lasttab = document.querySelector(".tab_bar>span.selected");
     if (lasttab==e.target)
